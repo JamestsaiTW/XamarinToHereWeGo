7aa75384e43a69524763e692a288a1af4226a1f3
James Tsai <Jatsai@msn.com>
2018-05-19 01:40:47 +0800

    整理 Xamarin.Android 的程式碼。並且加入串接 Waze 導航對照。

--- Xamarin.Native/Xamarin.Android/MainActivity.cs
+++ Xamarin.Native/Xamarin.Android/MainActivity.cs
@@ -4,12 +4,16 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Net;
 using Android.Content;
+using Android.Content.PM;
+using System.Threading.Tasks;
 
 namespace Xamarin.Android
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private string googlePlayStoreBaseUrl = "https://play.google.com/store/apps/details?id=";
+        private string targetLatLngStr = "25.033952,121.56447";
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -21,31 +25,88 @@ namespace Xamarin.Android
 
              googleMapButton.Click += (sender, args) =>
               {
-                  Uri gmmIntentUri = Uri.Parse("google.navigation:q=25.033952,121.564478&mode=d");
-                  //Ref
-                  //https://developers.google.com/maps/documentation/urls/android-intents
-                  //d for driving
-                  //w for walking
-                  //b for bicycling
+                  var packageName = "com.google.android.apps.maps";
+                  if (CheckAppIsInstalled(packageName))
+                  {
+                      var intentUriStr = $"google.navigation:q={targetLatLngStr}&mode=d";
+                      //Ref:
+                      //https://developers.google.com/maps/documentation/urls/android-intents
+                      //d for driving
+                      //w for walking
+                      //b for bicycling
 
-                  Intent mapIntent = new Intent(Intent.ActionView, gmmIntentUri);
-                  mapIntent.SetPackage("com.google.android.apps.maps");
-                  StartActivity(mapIntent);
+                      SwapToNavigatingMap(packageName, Intent.ActionView, intentUriStr);
+                      return;
+                  }
+                  GoToGooglePlayStoreDownload(packageName);
               };
 
             var hereWeGoButton = FindViewById<Button>(Resource.Id.HereWeGoButton);
 
             hereWeGoButton.Click += (sender, args) =>
             {
-                Uri gmmIntentUri = Uri.Parse("here.directions://v1.0/mylocation/25.033952,121.564478&m=d");
-                //Ref
-                //https://developer.here.com/documentation/mobility-on-demand-toolkit/topics/navigation.html
-                //d for driving
-                //w for walking
+                var packageName = "com.here.app.maps";
+                if (CheckAppIsInstalled(packageName))
+                {
+                    var intentUriStr = $"here.directions://v1.0/mylocation/{targetLatLngStr}&m=d";
+                    //Ref:
+                    //https://developer.here.com/documentation/mobility-on-demand-toolkit/topics/navigation.html
+                    //d for driving
+                    //w for walking
+                   
+                    SwapToNavigatingMap(packageName, "com.here.maps.DIRECTIONS", intentUriStr);
+                    return;
+                }
 
-                Intent mapIntent = new Intent("com.here.maps.DIRECTIONS", gmmIntentUri);
-                StartActivity(mapIntent);
+                GoToGooglePlayStoreDownload(packageName);
             };
+
+            var wazeButton = FindViewById<Button>(Resource.Id.WazeButton);
+
+            wazeButton.Click += (sender, args) =>
+            {
+                var packageName = "com.waze";
+                if (CheckAppIsInstalled(packageName))
+                {
+                    var intentUriStr = $"waze://?ll={targetLatLngStr}&navigate=yes";
+                    //Ref:
+                    //https://developers.google.com/waze/deeplinks/
+
+                    SwapToNavigatingMap(packageName, Intent.ActionView, intentUriStr);
+                    return;
+                }
+
+                GoToGooglePlayStoreDownload(packageName);
+            };
+        }
+
+        private async void GoToGooglePlayStoreDownload(string packageName)
+        {
+            Toast.MakeText(this, "在這台裝置上找不到該導航 App，將轉跳到 Google Play Store 下載此導航 App", ToastLength.Long).Show();
+            await Task.Delay(2000);
+            Intent mapIntent = new Intent(Intent.ActionView, Uri.Parse(googlePlayStoreBaseUrl+packageName));
+            StartActivity(mapIntent);
+        }
+
+        void SwapToNavigatingMap(string packageName, string action, string intentUri)
+        {
+            Intent mapIntent = new Intent(action, Uri.Parse(intentUri));
+            mapIntent.SetPackage(packageName);
+            StartActivity(mapIntent);
+        }
+
+        bool CheckAppIsInstalled(string packageName)
+        {
+            PackageManager pm = PackageManager;
+            try
+            {
+                PackageManager.GetPackageInfo(packageName, PackageInfoFlags.Activities);
+                return true;
+            }
+            catch (PackageManager.NameNotFoundException ex)
+            {
+                return false;
+            }
         }
     }
 }
--- Xamarin.Native/Xamarin.Android/Resources/Resource.Designer.cs
+++ Xamarin.Native/Xamarin.Android/Resources/Resource.Designer.cs
@@ -2227,26 +2227,26 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f02006a
-			public const int avd_hide_password_1 = 2130837610;
+			// aapt resource value: 0x7f02006d
+			public const int avd_hide_password_1 = 2130837613;
 			
-			// aapt resource value: 0x7f02006b
-			public const int avd_hide_password_2 = 2130837611;
+			// aapt resource value: 0x7f02006e
+			public const int avd_hide_password_2 = 2130837614;
 			
-			// aapt resource value: 0x7f02006c
-			public const int avd_hide_password_3 = 2130837612;
+			// aapt resource value: 0x7f02006f
+			public const int avd_hide_password_3 = 2130837615;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f02006d
-			public const int avd_show_password_1 = 2130837613;
+			// aapt resource value: 0x7f020070
+			public const int avd_show_password_1 = 2130837616;
 			
-			// aapt resource value: 0x7f02006e
-			public const int avd_show_password_2 = 2130837614;
+			// aapt resource value: 0x7f020071
+			public const int avd_show_password_2 = 2130837617;
 			
-			// aapt resource value: 0x7f02006f
-			public const int avd_show_password_3 = 2130837615;
+			// aapt resource value: 0x7f020072
+			public const int avd_show_password_3 = 2130837618;
 			
 			// aapt resource value: 0x7f020055
 			public const int design_bottom_navigation_item_background = 2130837589;
@@ -2267,49 +2267,58 @@ namespace Xamarin.Android
 			public const int design_snackbar_background = 2130837594;
 			
 			// aapt resource value: 0x7f02005b
-			public const int navigation_empty_icon = 2130837595;
+			public const int GoogleMap = 2130837595;
 			
 			// aapt resource value: 0x7f02005c
-			public const int notification_action_background = 2130837596;
+			public const int HereWeGo = 2130837596;
 			
 			// aapt resource value: 0x7f02005d
-			public const int notification_bg = 2130837597;
+			public const int navigation_empty_icon = 2130837597;
 			
 			// aapt resource value: 0x7f02005e
-			public const int notification_bg_low = 2130837598;
+			public const int notification_action_background = 2130837598;
 			
 			// aapt resource value: 0x7f02005f
-			public const int notification_bg_low_normal = 2130837599;
+			public const int notification_bg = 2130837599;
 			
 			// aapt resource value: 0x7f020060
-			public const int notification_bg_low_pressed = 2130837600;
+			public const int notification_bg_low = 2130837600;
 			
 			// aapt resource value: 0x7f020061
-			public const int notification_bg_normal = 2130837601;
+			public const int notification_bg_low_normal = 2130837601;
 			
 			// aapt resource value: 0x7f020062
-			public const int notification_bg_normal_pressed = 2130837602;
+			public const int notification_bg_low_pressed = 2130837602;
 			
 			// aapt resource value: 0x7f020063
-			public const int notification_icon_background = 2130837603;
-			
-			// aapt resource value: 0x7f020068
-			public const int notification_template_icon_bg = 2130837608;
-			
-			// aapt resource value: 0x7f020069
-			public const int notification_template_icon_low_bg = 2130837609;
+			public const int notification_bg_normal = 2130837603;
 			
 			// aapt resource value: 0x7f020064
-			public const int notification_tile_bg = 2130837604;
+			public const int notification_bg_normal_pressed = 2130837604;
 			
 			// aapt resource value: 0x7f020065
-			public const int notify_panel_notification_icon_bg = 2130837605;
+			public const int notification_icon_background = 2130837605;
+			
+			// aapt resource value: 0x7f02006b
+			public const int notification_template_icon_bg = 2130837611;
+			
+			// aapt resource value: 0x7f02006c
+			public const int notification_template_icon_low_bg = 2130837612;
 			
 			// aapt resource value: 0x7f020066
-			public const int tooltip_frame_dark = 2130837606;
+			public const int notification_tile_bg = 2130837606;
 			
 			// aapt resource value: 0x7f020067
-			public const int tooltip_frame_light = 2130837607;
+			public const int notify_panel_notification_icon_bg = 2130837607;
+			
+			// aapt resource value: 0x7f020068
+			public const int tooltip_frame_dark = 2130837608;
+			
+			// aapt resource value: 0x7f020069
+			public const int tooltip_frame_light = 2130837609;
+			
+			// aapt resource value: 0x7f02006a
+			public const int Waze = 2130837610;
 			
 			static Drawable()
 			{
@@ -2348,8 +2357,11 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f080037
 			public const int SYM = 2131230775;
 			
-			// aapt resource value: 0x7f08009f
-			public const int action0 = 2131230879;
+			// aapt resource value: 0x7f08008e
+			public const int WazeButton = 2131230862;
+			
+			// aapt resource value: 0x7f0800a0
+			public const int action0 = 2131230880;
 			
 			// aapt resource value: 0x7f08007c
 			public const int action_bar = 2131230844;
@@ -2372,17 +2384,17 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f08005a
 			public const int action_bar_title = 2131230810;
 			
-			// aapt resource value: 0x7f08009c
-			public const int action_container = 2131230876;
+			// aapt resource value: 0x7f08009d
+			public const int action_container = 2131230877;
 			
 			// aapt resource value: 0x7f08007d
 			public const int action_context_bar = 2131230845;
 			
-			// aapt resource value: 0x7f0800a3
-			public const int action_divider = 2131230883;
+			// aapt resource value: 0x7f0800a4
+			public const int action_divider = 2131230884;
 			
-			// aapt resource value: 0x7f08009d
-			public const int action_image = 2131230877;
+			// aapt resource value: 0x7f08009e
+			public const int action_image = 2131230878;
 			
 			// aapt resource value: 0x7f080003
 			public const int action_menu_divider = 2131230723;
@@ -2399,11 +2411,11 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f08005c
 			public const int action_mode_close_button = 2131230812;
 			
-			// aapt resource value: 0x7f08009e
-			public const int action_text = 2131230878;
+			// aapt resource value: 0x7f08009f
+			public const int action_text = 2131230879;
 			
-			// aapt resource value: 0x7f0800ac
-			public const int actions = 2131230892;
+			// aapt resource value: 0x7f0800ad
+			public const int actions = 2131230893;
 			
 			// aapt resource value: 0x7f08005d
 			public const int activity_chooser_view_content = 2131230813;
@@ -2438,8 +2450,8 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f080063
 			public const int buttonPanel = 2131230819;
 			
-			// aapt resource value: 0x7f0800a0
-			public const int cancel_action = 2131230880;
+			// aapt resource value: 0x7f0800a1
+			public const int cancel_action = 2131230881;
 			
 			// aapt resource value: 0x7f080045
 			public const int center = 2131230789;
@@ -2453,8 +2465,8 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f080073
 			public const int checkbox = 2131230835;
 			
-			// aapt resource value: 0x7f0800a8
-			public const int chronometer = 2131230888;
+			// aapt resource value: 0x7f0800a9
+			public const int chronometer = 2131230889;
 			
 			// aapt resource value: 0x7f08004e
 			public const int clip_horizontal = 2131230798;
@@ -2465,14 +2477,14 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f080039
 			public const int collapseActionView = 2131230777;
 			
-			// aapt resource value: 0x7f080090
-			public const int container = 2131230864;
+			// aapt resource value: 0x7f080091
+			public const int container = 2131230865;
 			
 			// aapt resource value: 0x7f080066
 			public const int contentPanel = 2131230822;
 			
-			// aapt resource value: 0x7f080091
-			public const int coordinator = 2131230865;
+			// aapt resource value: 0x7f080092
+			public const int coordinator = 2131230866;
 			
 			// aapt resource value: 0x7f08006d
 			public const int custom = 2131230829;
@@ -2486,20 +2498,20 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f080060
 			public const int default_activity_button = 2131230816;
 			
-			// aapt resource value: 0x7f080093
-			public const int design_bottom_sheet = 2131230867;
+			// aapt resource value: 0x7f080094
+			public const int design_bottom_sheet = 2131230868;
+			
+			// aapt resource value: 0x7f08009b
+			public const int design_menu_item_action_area = 2131230875;
 			
 			// aapt resource value: 0x7f08009a
-			public const int design_menu_item_action_area = 2131230874;
+			public const int design_menu_item_action_area_stub = 2131230874;
 			
 			// aapt resource value: 0x7f080099
-			public const int design_menu_item_action_area_stub = 2131230873;
+			public const int design_menu_item_text = 2131230873;
 			
 			// aapt resource value: 0x7f080098
-			public const int design_menu_item_text = 2131230872;
-			
-			// aapt resource value: 0x7f080097
-			public const int design_navigation_view = 2131230871;
+			public const int design_navigation_view = 2131230872;
 			
 			// aapt resource value: 0x7f080020
 			public const int disableHome = 2131230752;
@@ -2510,8 +2522,8 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f080030
 			public const int end = 2131230768;
 			
-			// aapt resource value: 0x7f0800ae
-			public const int end_padder = 2131230894;
+			// aapt resource value: 0x7f0800af
+			public const int end_padder = 2131230895;
 			
 			// aapt resource value: 0x7f08003f
 			public const int enterAlways = 2131230783;
@@ -2555,8 +2567,8 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f080062
 			public const int icon = 2131230818;
 			
-			// aapt resource value: 0x7f0800ad
-			public const int icon_group = 2131230893;
+			// aapt resource value: 0x7f0800ae
+			public const int icon_group = 2131230894;
 			
 			// aapt resource value: 0x7f08003a
 			public const int ifRoom = 2131230778;
@@ -2564,8 +2576,8 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f08005f
 			public const int image = 2131230815;
 			
-			// aapt resource value: 0x7f0800a9
-			public const int info = 2131230889;
+			// aapt resource value: 0x7f0800aa
+			public const int info = 2131230890;
 			
 			// aapt resource value: 0x7f080059
 			public const int italic = 2131230809;
@@ -2573,8 +2585,8 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f080000
 			public const int item_touch_helper_previous_elevation = 2131230720;
 			
-			// aapt resource value: 0x7f08008f
-			public const int largeLabel = 2131230863;
+			// aapt resource value: 0x7f080090
+			public const int largeLabel = 2131230864;
 			
 			// aapt resource value: 0x7f080049
 			public const int left = 2131230793;
@@ -2591,14 +2603,14 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f080061
 			public const int list_item = 2131230817;
 			
-			// aapt resource value: 0x7f0800b1
-			public const int masked = 2131230897;
+			// aapt resource value: 0x7f0800b2
+			public const int masked = 2131230898;
 			
-			// aapt resource value: 0x7f0800a2
-			public const int media_actions = 2131230882;
+			// aapt resource value: 0x7f0800a3
+			public const int media_actions = 2131230883;
 			
-			// aapt resource value: 0x7f0800af
-			public const int message = 2131230895;
+			// aapt resource value: 0x7f0800b0
+			public const int message = 2131230896;
 			
 			// aapt resource value: 0x7f080031
 			public const int middle = 2131230769;
@@ -2609,8 +2621,8 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f080028
 			public const int multiply = 2131230760;
 			
-			// aapt resource value: 0x7f080096
-			public const int navigation_header_container = 2131230870;
+			// aapt resource value: 0x7f080097
+			public const int navigation_header_container = 2131230871;
 			
 			// aapt resource value: 0x7f08003b
 			public const int never = 2131230779;
@@ -2621,14 +2633,14 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f08001e
 			public const int normal = 2131230750;
 			
-			// aapt resource value: 0x7f0800ab
-			public const int notification_background = 2131230891;
+			// aapt resource value: 0x7f0800ac
+			public const int notification_background = 2131230892;
+			
+			// aapt resource value: 0x7f0800a6
+			public const int notification_main_column = 2131230886;
 			
 			// aapt resource value: 0x7f0800a5
-			public const int notification_main_column = 2131230885;
-			
-			// aapt resource value: 0x7f0800a4
-			public const int notification_main_column_container = 2131230884;
+			public const int notification_main_column_container = 2131230885;
 			
 			// aapt resource value: 0x7f08004c
 			public const int parallax = 2131230796;
@@ -2654,11 +2666,11 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f08004a
 			public const int right = 2131230794;
 			
-			// aapt resource value: 0x7f0800aa
-			public const int right_icon = 2131230890;
+			// aapt resource value: 0x7f0800ab
+			public const int right_icon = 2131230891;
 			
-			// aapt resource value: 0x7f0800a6
-			public const int right_side = 2131230886;
+			// aapt resource value: 0x7f0800a7
+			public const int right_side = 2131230887;
 			
 			// aapt resource value: 0x7f08000c
 			public const int save_image_matrix = 2131230732;
@@ -2732,14 +2744,14 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f080025
 			public const int showTitle = 2131230757;
 			
-			// aapt resource value: 0x7f08008e
-			public const int smallLabel = 2131230862;
+			// aapt resource value: 0x7f08008f
+			public const int smallLabel = 2131230863;
+			
+			// aapt resource value: 0x7f080096
+			public const int snackbar_action = 2131230870;
 			
 			// aapt resource value: 0x7f080095
-			public const int snackbar_action = 2131230869;
-			
-			// aapt resource value: 0x7f080094
-			public const int snackbar_text = 2131230868;
+			public const int snackbar_text = 2131230869;
 			
 			// aapt resource value: 0x7f080043
 			public const int snap = 2131230787;
@@ -2762,8 +2774,8 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f08004b
 			public const int start = 2131230795;
 			
-			// aapt resource value: 0x7f0800a1
-			public const int status_bar_latest_event_content = 2131230881;
+			// aapt resource value: 0x7f0800a2
+			public const int status_bar_latest_event_content = 2131230882;
 			
 			// aapt resource value: 0x7f080076
 			public const int submenuarrow = 2131230838;
@@ -2792,8 +2804,8 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f08008b
 			public const int textView1 = 2131230859;
 			
-			// aapt resource value: 0x7f08009b
-			public const int text_input_password_toggle = 2131230875;
+			// aapt resource value: 0x7f08009c
+			public const int text_input_password_toggle = 2131230876;
 			
 			// aapt resource value: 0x7f080014
 			public const int textinput_counter = 2131230740;
@@ -2801,8 +2813,8 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f080015
 			public const int textinput_error = 2131230741;
 			
-			// aapt resource value: 0x7f0800a7
-			public const int time = 2131230887;
+			// aapt resource value: 0x7f0800a8
+			public const int time = 2131230888;
 			
 			// aapt resource value: 0x7f08001c
 			public const int title = 2131230748;
@@ -2819,8 +2831,8 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f08006e
 			public const int topPanel = 2131230830;
 			
-			// aapt resource value: 0x7f080092
-			public const int touch_outside = 2131230866;
+			// aapt resource value: 0x7f080093
+			public const int touch_outside = 2131230867;
 			
 			// aapt resource value: 0x7f08000f
 			public const int transition_current_scene = 2131230735;
@@ -2849,8 +2861,8 @@ namespace Xamarin.Android
 			// aapt resource value: 0x7f080016
 			public const int view_offset_helper = 2131230742;
 			
-			// aapt resource value: 0x7f0800b0
-			public const int visible = 2131230896;
+			// aapt resource value: 0x7f0800b1
+			public const int visible = 2131230897;
 			
 			// aapt resource value: 0x7f08003c
 			public const int withText = 2131230780;
